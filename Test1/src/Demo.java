cc6f792a40bf7e471b68cead0e46629e2168ae9a
HarishKumarGudivada <33482640+HarishKumarGudivada@users.noreply.github.com>
2017-11-20 16:58:39 +0530

    Update Demo.java

--- Test1/src/Demo.java
+++ Test1/src/Demo.java
@@ -4,7 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Branch 1 code");
 		System.out.println(" Update Branch 1 code");
-		System.out.println(" So far So good");
 	}
 
 	
